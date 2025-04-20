31c4325ec90e106ae143bce3d22757e87bfb8536
Amit_Vaxberg <amit.vaxberg@gmail.com>
2025-04-20 21:50:03 +0300

    Second question solved

--- hw2_q2.py
+++ hw2_q2.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from enum import Enum
+from itertools import batched
 
 Condition = Enum("Condition", ("CURE", "HEALTHY", "SICK", "DYING", "DEAD"))
 Agent = namedtuple("Agent", ("name", "category"))
@@ -29,3 +30,48 @@ def meetup(agent_listing: tuple) -> list:
         A list of Agents with their 'category' field changed according to the result
         of the meeting.
     """
+
+    filterd_agents = filter(lambda agent: agent.category != Condition.HEALTHY and agent.category != Condition.DEAD, agent_listing)
+    non_participated_agents = filter(lambda agent: agent.category == Condition.HEALTHY or agent.category == Condition.DEAD, agent_listing)
+    results = []
+    results += list(non_participated_agents)
+    
+    meetings = batched(filterd_agents, n=2)
+    for agents in meetings:
+        if len(agents) != 2:
+            results.append(agents[0])
+            break
+
+        if should_demote(agents[0], agents[1]):
+            results.append(Agent(name=agents[1].name, category=Condition(agents[1].category.value + 1)))
+        elif should_promote(agents[0], agents[1]):
+            results.append(Agent(name=agents[1].name, category=Condition(agents[1].category.value - 1)))
+        else:
+            results.append(agents[1])
+
+        if should_demote(agents[1], agents[0]):
+            results.append(Agent(name=agents[0].name, category=Condition(agents[0].category.value + 1)))
+        elif should_promote(agents[1], agents[0]):
+            results.append(Agent(name=agents[0].name, category=Condition(agents[0].category.value - 1)))
+        else:
+            results.append(agents[0])
+
+    return results
+
+
+def should_demote(agent1, agent2):
+    return (agent1.category == Condition.SICK or agent1.category == Condition.DYING) and agent2.category != Condition.CURE
+
+
+def should_promote(agent1, agent2):
+    return agent1.category == Condition.CURE and agent2.category != Condition.CURE
+
+if  __name__ == '__main__':
+    # Question 2
+    data0 = (Agent("Buddy", Condition.CURE), Agent("Holly", Condition.DEAD))
+    return_value = meetup(data0)
+    print(f"Question 2 solution: {return_value}")
+    
+            
+    
+
